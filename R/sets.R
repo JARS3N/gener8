01fbe3f7ae75be4d598a9937ecdb5d29d1a8ba8d
JARS3N <JARS3N@users.noreply.github.com>
2019-01-28 00:22:08 -0500

    Update and rename sample_sets.R to sets.R

--- R/sets.R
+++ R/sets.R
@@ -1,10 +1,10 @@
-sample_sets<-function(elements,size,blocksize,rssreps=1000){
+sets<-function(elements,size,blocksize,rssreps=1000){
   #elements  vector to split and create blocks of samples for
   # per line sample size
   # blocks: number of lines sampled per element
   # rss to find best distribution of elements
   all<-lapply(elements,function(u)(blocks(elements,u,size,blocksize)))
-  replicates<-lapply(1:rssreps,function(i){all_unique_combinations(all,length(elements),blocksize)})
+  replicates<-lapply(1:rssreps,function(i){unique_sets(all,length(elements),blocksize)})
   rng<-unlist(lapply(replicates,function(u){diff(range(table(unlist(u))))}))
   rss<-unlist(lapply(replicates,function(u){
     x<-table(unlist(u))-(size*blocksize)
